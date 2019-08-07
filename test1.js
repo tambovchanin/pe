b7f70f7b99eb1d8f4965b3f45540576e0a219af5
Denis Shiryaev <tambovchanin@gmail.com>
2019-08-07 23:18:08 +0300

    Handle errors

--- test1.js
+++ test1.js
@@ -1,6 +1,7 @@
 const pe = require('./lib/pe');
 const { readFileSync} = require('fs');
-const FILE = './bin/ctc-wind10.exe';
+const FILE = './bin/node32.exe';
+// const FILE = './bin/ctc-wind10.exe';
 // const FILE = './bin/libegl.dll';
 // const FILE = './bin/d3dcompiler_47.dll';
 
@@ -14,7 +15,9 @@ bi.sections = pef.sectionHeaders;
 
 for(let kind in pe.headers.DataDirectoryKind) {
   if (pef.optionalHeader.dataDirectories[kind])
-    console.log(pe.headers.DataDirectoryKind[kind], 'address:', pef.optionalHeader.dataDirectories[kind].address);
+    console.log(pe.headers.DataDirectoryKind[kind],
+      'address:', pef.optionalHeader.dataDirectories[kind].address,
+      'size', pef.optionalHeader.dataDirectories[kind].size);
 }
 
 var importRange = pef.optionalHeader.dataDirectories[pe.headers.DataDirectoryKind.ImportSymbols];
@@ -23,20 +26,34 @@ var expRange = pef.optionalHeader.dataDirectories[pe.headers.DataDirectoryKind.E
 
 if (importRange.address) {
   bi.setVirtualOffset(importRange.address);
-  let imports = pe.unmanaged.DllImport.read(bi);
-  console.log('* Imports:\n', imports);
+  try {
+    let imports = pe.unmanaged.DllImport.read(bi);
+    console.log('* Imports:\n', imports);
+  } catch(err) {
+    console.log('! Can\'t get imports');
+  }
+
 }
 
 if (resRange.address) {
   var resources = new pe.unmanaged.ResourceDirectory();
   bi.setVirtualOffset(resRange.address);
-  resources.read(bi);
-  console.log('* Resources:\n', resources);
+  try {
+    resources.read(bi);
+    console.log('* Resources:\n', resources);
+  } catch(err) {
+    console.log('! Can\'t get resources');
+  }
 
 }
 
 if (expRange.address) {
   bi.setVirtualOffset(expRange.address);
-  var exp = pe.unmanaged.DllExport.readExports(bi, expRange);
-  console.log('* Exports:\n', exp);
+
+  try {
+    var exp = pe.unmanaged.DllExport.readExports(bi, expRange);
+    console.log('* Exports:\n', exp);
+  } catch(err) {
+    console.log('! Can\'t get exports');
+  }
 }
